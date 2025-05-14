5fe2cbff06afdd563d0b3b212210fc24c3a8e9e9
alison <adevpr11@gmail.com>
2025-05-14 19:22:26 -0300

    Adiciona a classe AlunoRepository

---
+++ src/repositories/AlunoRepository.java
@@ -0,0 +1,85 @@
+package repositories;
+
+import java.util.UUID;
+
+import entities.Aluno;
+import factories.ConnectionFactory;
+
+public class AlunoRepository {
+
+	ConnectionFactory connectionFactory = new ConnectionFactory();
+
+	public void inserir(Aluno aluno) {
+		try {
+			var connection = connectionFactory.obterConexao();
+			var statement = connection
+					.prepareStatement("insert into aluno (id, nome, matricula, cpf) values(?,?,?,?)");
+			statement.setObject(1, aluno.getId());
+			statement.setString(2, aluno.getNome());
+			statement.setString(3, aluno.getMatricula());
+			statement.setString(4, aluno.getCpf());
+			statement.execute();
+
+			connection.close();
+			System.out.println("\nAluno inserido com sucesso!");
+		} catch (Exception e) {
+			System.out.println("\nErro ao inserir aluno: " + e.getMessage());
+		}
+	}
+
+	public void alterar(Aluno aluno) {
+		try {
+
+			var connection = connectionFactory.obterConexao();
+			var statement = connection.prepareStatement("update aluno set nome=?, matricula=?, cpf=? where id=?");
+
+			statement.setString(1, aluno.getNome());
+			statement.setString(2, aluno.getMatricula());
+			statement.setString(3, aluno.getCpf());
+			statement.setObject(4, aluno.getId());
+			statement.execute();
+
+			connection.close();
+			System.out.println("\nAluno alterado com sucesso!");
+		} catch (Exception e) {
+			System.out.println("\nErro ao alterar aluno: " + e.getMessage());
+		}
+	}
+
+	public void excluir(UUID id) {
+		try {
+			var connection = connectionFactory.obterConexao();
+			var statement = connection.prepareStatement("delete from aluno where id=?");
+
+			statement.setObject(1, id);
+			statement.execute();
+
+			connection.close();
+			System.out.println("\nAluno excluído com sucesso!");
+		} catch (Exception e) {
+			System.out.println("\nErro ao excluir aluno: " + e.getMessage());
+		}
+	}
+
+	public void consultar() {
+		try {
+			var connection = connectionFactory.obterConexao();
+			var statement = connection.prepareStatement("select id, nome, matricula, cpf from aluno");
+			var resultSet = statement.executeQuery();
+			
+			if (resultSet != null) {
+				while (resultSet.next()) {
+					System.out.println("\nID.............: " + resultSet.getObject("id"));
+					System.out.println("NOME...........: " + resultSet.getString("nome"));
+					System.out.println("MATRICULA..........: " + resultSet.getString("matricula"));
+					System.out.println("CPF.....: " + resultSet.getString("cpf"));
+				}
+			}
+
+			connection.close();
+			System.out.println("\nConsulta realizada com sucesso!");
+		} catch (Exception e) {
+			System.out.println("\nErro ao consultar aluno: " + e.getMessage());
+		}
+	}
+}
