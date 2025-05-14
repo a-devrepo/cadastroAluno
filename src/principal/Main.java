a2f715ed8830cd133edb99408eb4df63fa586481
alison <adevpr11@gmail.com>
2025-05-14 19:35:53 -0300

    Adiciona classe Main

---
+++ src/principal/Main.java
@@ -0,0 +1,12 @@
+package principal;
+
+import controllers.AlunoController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		var alunoController = new AlunoController();
+		alunoController.cadastrarAluno();
+	}
+
+}
