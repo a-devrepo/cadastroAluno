2284c84cd534da7674933b65135121b4989377a5
alison <adevpr11@gmail.com>
2025-05-16 15:45:06 -0300

    Altera a classe Main para fazer injeção de dependência na classe AlunoRepository

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -2,17 +2,18 @@ package principal;
 
 import controllers.AlunoController;
 import controllers.views.ConsoleOutput;
+import factories.ConnectionFactory;
 import repositories.AlunoRepository;
 import services.AlunoService;
 
 public class Main {
 
 	public static void main(String[] args) {
-		var alunoRepository = new AlunoRepository();
+		var connectionFactory = new ConnectionFactory();
+		var alunoRepository = new AlunoRepository(connectionFactory);
 		var alunoService = new AlunoService(alunoRepository);
 		var consoleOutput = new ConsoleOutput();
 		var alunoController = new AlunoController(alunoService, consoleOutput);
 		alunoController.exibirOpcoes();
 	}
-
 }
