3c0132c5134d7acc2be87e745b7cf043cdd31096
alison <adevpr11@gmail.com>
2025-05-16 12:34:35 -0300

    Altera lógica de criação dos objetos na classe Main para usar injeção de dependência

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,11 +1,17 @@
 package principal;
 
 import controllers.AlunoController;
+import repositories.AlunoRepository;
+import services.AlunoService;
+import views.ConsoleOutput;
 
 public class Main {
 
 	public static void main(String[] args) {
-		var alunoController = new AlunoController();
+		var alunoRepository = new AlunoRepository();
+		var alunoService = new AlunoService(alunoRepository);
+		var consoleOutput = new ConsoleOutput();
+		var alunoController = new AlunoController(alunoService, consoleOutput);
 		alunoController.exibirOpcoes();
 	}
 
