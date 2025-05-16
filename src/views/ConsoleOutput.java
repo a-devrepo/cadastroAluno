e37f97dea40e1596ce9ffe8ccdc8e3fca3df7301
alison <adevpr11@gmail.com>
2025-05-16 13:21:34 -0300

    Acrescenta método para exibir com quebra de linha em ConsoleOutput

--- src/views/ConsoleOutput.java
+++ src/views/ConsoleOutput.java
@@ -9,4 +9,8 @@ public class ConsoleOutput {
 	public void exibirTextoParaEntrada(String texto) {
 		System.out.print(texto);
 	}
+
+	public void exibirComQuebraLinha(String texto) {
+		System.out.println("\n" + texto);
+	}
 }
