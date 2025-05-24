f66cbcac5125fd2a3eae5c29041669c6d50f7bbe
alison <adevpr11@gmail.com>
2025-05-24 18:54:24 -0300

    Altera a assinatura dos métodos de AlunoValidator

--- src/controllers/validators/AlunoValidator.java
+++ src/controllers/validators/AlunoValidator.java
@@ -6,7 +6,7 @@ import exceptions.DadosInvalidosException;
 
 public class AlunoValidator {
 
-	public static void validarId(String id) {
+	public void validarId(String id) {
 		if (id == null || id.isBlank()) {
 			throw new DadosInvalidosException("ID inválido");
 		}
@@ -17,24 +17,24 @@ public class AlunoValidator {
 		}
 	}
 
-	public static void validarNome(String nome) {
+	public void validarNome(String nome) {
 		if (nome == null || nome.isBlank() || nome.length() < 3) {
 			throw new DadosInvalidosException("Nome inválido");
 		}
 	}
 
-	public static void validarMatricula(String matricula) {
+	public void validarMatricula(String matricula) {
 		if (matricula == null || matricula.isBlank()) {
 			throw new DadosInvalidosException("Matrícula inválida");
 		}
 	}
 
-	public static void validarCpf(String cpf) {
+	public void validarCpf(String cpf) {
 		if (cpf == null || cpf.isBlank() || cpf.length() != 11) {
 			throw new DadosInvalidosException("CPF inválido");
 		}
 		if (!cpf.matches("\\d+")) {
 			throw new DadosInvalidosException("CPF deve conter apenas números");
-		}	 
+		}
 	}
 }
