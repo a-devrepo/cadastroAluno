c2e1825755302dfe0dd7760a63ca86ce1997b90d
alison <adevpr11@gmail.com>
2025-05-25 18:02:28 -0300

    Adiciona a classe DomainException

---
+++ src/exceptions/DomainException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class DomainException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DomainException(String message) {
+		super(message);
+	}
+
+	public DomainException(String message, Throwable throwable) {
+	}
+}
