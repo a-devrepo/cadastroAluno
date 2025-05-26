32b5404d7edc15243425e903271c772986ac74ef
alison <adevpr11@gmail.com>
2025-05-26 17:58:37 -0300

    Correção da injeção de objetos nas classes ConsoleInput e AlunoController

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,8 +1,12 @@
 package principal;
 
+import java.util.Scanner;
+
 import controllers.AlunoController;
-import controllers.validators.AlunoValidator;
+import controllers.validators.InputValidator;
+import controllers.views.ConsoleInput;
 import controllers.views.ConsoleOutput;
+import factories.AlunoFactory;
 import factories.PostgreConnectionFactory;
 import repositories.AlunoRepository;
 import services.AlunoService;
@@ -10,12 +14,15 @@ import services.AlunoService;
 public class Main {
 
 	public static void main(String[] args) {
+		var scanner = new Scanner(System.in);
 		var dataBaseConnection = new PostgreConnectionFactory().criarDatabaseConnection();
 		var alunoRepository = new AlunoRepository(dataBaseConnection);
 		var alunoService = new AlunoService(alunoRepository);
 		var consoleOutput = new ConsoleOutput();
-		var alunoValidator = new AlunoValidator();
-		var alunoController = new AlunoController(alunoService, consoleOutput,alunoValidator);
+		var consoleInput = new ConsoleInput(scanner, consoleOutput);
+		var alunoValidator = new InputValidator();
+		var alunoFactory = new AlunoFactory();
+		var alunoController = new AlunoController(scanner,alunoService, consoleOutput,alunoValidator, consoleInput, alunoFactory);
 		alunoController.exibirOpcoes();
 	}
 }
