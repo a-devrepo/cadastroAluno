0cd5cd24cbd5925bfff28bfdbf416c71b5c8b1cc
alison <adevpr11@gmail.com>
2025-05-26 17:30:05 -0300

    Adiciona classe AlunoFactory

---
+++ src/factories/AlunoFactory.java
@@ -0,0 +1,12 @@
+package factories;
+
+import java.util.UUID;
+
+import entities.Aluno;
+
+public class AlunoFactory {
+
+	public Aluno criar(String nome, String cpf, String matricula) {
+		return new Aluno(UUID.randomUUID(), nome, cpf, matricula);
+	}
+}
